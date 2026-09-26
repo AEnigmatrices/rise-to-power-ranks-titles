3c3f070c0add3f8a62e72481e65d25e34452b715
AEnigmatrices <69204408+AEnigmatrices@users.noreply.github.com>
2026-09-26 07:29:33 +0700

    Include imported item data in the completed branch tree

---
+++ src/data/items/art.ts
@@ -0,0 +1,605 @@
+import type { Item } from './types';
+
+/** Imported from the supplied art HTML catalogue; game labels are preserved verbatim. */
+export const artItems: Item[] = [
+    {
+        "id": "art-painting-8-fold-painting",
+        "type": "Painting",
+        "name": "8 Fold Painting",
+        "japanese": "瀟湘八景図",
+        "quality": 10,
+        "origin": "China",
+        "effect": "Charm + 10",
+        "sourceOrder": 0
+    },
+    {
+        "id": "art-painting-famous-painting",
+        "type": "Painting",
+        "name": "Famous Painting",
+        "japanese": "破墨山水図",
+        "quality": 9,
+        "origin": "Japan",
+        "effect": "Charm + 9",
+        "sourceOrder": 1
+    },
+    {
+        "id": "art-painting-cloth-bag-painting",
+        "type": "Painting",
+        "name": "Cloth Bag Painting",
+        "japanese": "布袋図",
+        "quality": 8,
+        "origin": "China",
+        "effect": "Charm + 8",
+        "sourceOrder": 2
+    },
+    {
+        "id": "art-painting-daruma-painting",
+        "type": "Painting",
+        "name": "Daruma Painting",
+        "japanese": "松下達磨図",
+        "quality": 7,
+        "origin": "China",
+        "effect": "Charm + 7",
+        "sourceOrder": 3
+    },
+    {
+        "id": "art-painting-landscape-painting",
+        "type": "Painting",
+        "name": "Landscape Painting",
+        "japanese": "松鷹図",
+        "quality": 6,
+        "origin": "Japan",
+        "effect": "Charm + 6",
+        "sourceOrder": 4
+    },
+    {
+        "id": "art-painting-mercy-painting",
+        "type": "Painting",
+        "name": "Mercy Painting",
+        "japanese": "観音図",
+        "quality": 5,
+        "origin": "China",
+        "effect": "Charm + 5",
+        "sourceOrder": 5
+    },
+    {
+        "id": "art-painting-stone-iris-painting",
+        "type": "Painting",
+        "name": "Stone Iris Painting",
+        "japanese": "石菖蒲図",
+        "quality": 4,
+        "origin": "China",
+        "effect": "Charm + 4",
+        "sourceOrder": 6
+    },
+    {
+        "id": "art-painting-night-dew-painting",
+        "type": "Painting",
+        "name": "Night Dew Painting",
+        "japanese": "芭蕉夜雨図",
+        "quality": 3,
+        "origin": "Japan",
+        "effect": "Charm + 3",
+        "sourceOrder": 7
+    },
+    {
+        "id": "art-painting-reader-s-painting",
+        "type": "Painting",
+        "name": "Reader's Painting",
+        "japanese": "竹斎読書図",
+        "quality": 2,
+        "origin": "China",
+        "effect": "Charm + 2",
+        "sourceOrder": 8
+    },
+    {
+        "id": "art-painting-chestnut-painting",
+        "type": "Painting",
+        "name": "Chestnut Painting",
+        "japanese": "栗柿図",
+        "quality": 1,
+        "origin": "China",
+        "effect": "Charm + 1",
+        "sourceOrder": 9
+    },
+    {
+        "id": "art-art-heron-art",
+        "type": "Art",
+        "name": "Heron Art",
+        "japanese": "白鷺図",
+        "quality": 10,
+        "origin": "China",
+        "effect": "Charm + 10",
+        "sourceOrder": 10
+    },
+    {
+        "id": "art-art-four-petal-art",
+        "type": "Art",
+        "name": "Four Petal Art",
+        "japanese": "四季花鳥図",
+        "quality": 9,
+        "origin": "Japan",
+        "effect": "Charm + 9",
+        "sourceOrder": 11
+    },
+    {
+        "id": "art-art-pastry-art",
+        "type": "Art",
+        "name": "Pastry Art",
+        "japanese": "菓子図",
+        "quality": 8,
+        "origin": "China",
+        "effect": "Charm + 8",
+        "sourceOrder": 12
+    },
+    {
+        "id": "art-art-buddhist-tome-art",
+        "type": "Art",
+        "name": "Buddhist Tome Art",
+        "japanese": "釈迦説法図",
+        "quality": 7,
+        "origin": "Japan",
+        "effect": "Charm + 7",
+        "sourceOrder": 13
+    },
+    {
+        "id": "art-art-rice-art",
+        "type": "Art",
+        "name": "Rice Art",
+        "japanese": "網干図",
+        "quality": 6,
+        "origin": "Japan",
+        "effect": "Charm + 6",
+        "sourceOrder": 14
+    },
+    {
+        "id": "art-art-duck-art",
+        "type": "Art",
+        "name": "Duck Art",
+        "japanese": "鴨図",
+        "quality": 5,
+        "origin": "China",
+        "effect": "Charm + 5",
+        "sourceOrder": 15
+    },
+    {
+        "id": "art-art-rough-seas-art",
+        "type": "Art",
+        "name": "Rough Seas Art",
+        "japanese": "波濤図",
+        "quality": 4,
+        "origin": "Japan",
+        "effect": "Charm + 4",
+        "sourceOrder": 16
+    },
+    {
+        "id": "art-art-sparrow-art",
+        "type": "Art",
+        "name": "Sparrow Art",
+        "japanese": "稲雀図",
+        "quality": 3,
+        "origin": "China",
+        "effect": "Charm + 3",
+        "sourceOrder": 17
+    },
+    {
+        "id": "art-art-red-plum-art",
+        "type": "Art",
+        "name": "Red Plum Art",
+        "japanese": "紅梅図",
+        "quality": 2,
+        "origin": "Japan",
+        "effect": "Charm + 2",
+        "sourceOrder": 18
+    },
+    {
+        "id": "art-art-sunset-art",
+        "type": "Art",
+        "name": "Sunset Art",
+        "japanese": "夕陽朝山図",
+        "quality": 1,
+        "origin": "China",
+        "effect": "Charm + 1",
+        "sourceOrder": 19
+    },
+    {
+        "id": "art-sweets-walnut-cake",
+        "type": "Sweets(笑)",
+        "name": "Walnut Cake",
+        "japanese": "胡桃餅",
+        "quality": 10,
+        "origin": "Japan",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 20
+    },
+    {
+        "id": "art-sweets-barley-candy",
+        "type": "Sweets(笑)",
+        "name": "Barley Candy",
+        "japanese": "麦落雁",
+        "quality": 9,
+        "origin": "Japan",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 21
+    },
+    {
+        "id": "art-sweets-castella",
+        "type": "Sweets(笑)",
+        "name": "Castella",
+        "japanese": "カステラ",
+        "quality": 8,
+        "origin": "Europe",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 22
+    },
+    {
+        "id": "art-sweets-sweet-jelly",
+        "type": "Sweets(笑)",
+        "name": "Sweet Jelly",
+        "japanese": "薄墨羊羹",
+        "quality": 7,
+        "origin": "Japan",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 23
+    },
+    {
+        "id": "art-sweets-senbei",
+        "type": "Sweets(笑)",
+        "name": "Senbei",
+        "japanese": "寿煎餅",
+        "quality": 6,
+        "origin": "Japan",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 24
+    },
+    {
+        "id": "art-sweets-biscuit",
+        "type": "Sweets(笑)",
+        "name": "Biscuit",
+        "japanese": "ビスケット",
+        "quality": 5,
+        "origin": "Europe",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 25
+    },
+    {
+        "id": "art-sweets-willow-treat",
+        "type": "Sweets(笑)",
+        "name": "Willow Treat",
+        "japanese": "青柳",
+        "quality": 4,
+        "origin": "Japan",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 26
+    },
+    {
+        "id": "art-sweets-moon-sweets",
+        "type": "Sweets(笑)",
+        "name": "Moon Sweets",
+        "japanese": "朧月",
+        "quality": 3,
+        "origin": "Japan",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 27
+    },
+    {
+        "id": "art-sweets-plain-candy",
+        "type": "Sweets(笑)",
+        "name": "Plain Candy",
+        "japanese": "金平糖",
+        "quality": 2,
+        "origin": "Europe",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 28
+    },
+    {
+        "id": "art-sweets-sugar",
+        "type": "Sweets(笑)",
+        "name": "Sugar",
+        "japanese": "吐月糖",
+        "quality": 1,
+        "origin": "Japan",
+        "effect": "Skill[Rally]",
+        "sourceOrder": 29
+    },
+    {
+        "id": "art-jizake-rose-wine",
+        "type": "Jizake",
+        "name": "Rose Wine",
+        "japanese": "薔薇葡萄酒",
+        "quality": 10,
+        "origin": "Europe",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 30
+    },
+    {
+        "id": "art-jizake-whiskey",
+        "type": "Jizake",
+        "name": "Whiskey",
+        "japanese": "アクアヴィテ",
+        "quality": 9,
+        "origin": "Europe",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 31
+    },
+    {
+        "id": "art-jizake-sparkling-wine",
+        "type": "Jizake",
+        "name": "Sparkling Wine",
+        "japanese": "発泡酒",
+        "quality": 8,
+        "origin": "Europe",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 32
+    },
+    {
+        "id": "art-jizake-white-wine",
+        "type": "Jizake",
+        "name": "White Wine",
+        "japanese": "白葡萄酒",
+        "quality": 7,
+        "origin": "Europe",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 33
+    },
+    {
+        "id": "art-jizake-cognac",
+        "type": "Jizake",
+        "name": "Cognac",
+        "japanese": "菩提泉",
+        "quality": 6,
+        "origin": "Japan",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 34
+    },
+    {
+        "id": "art-jizake-cider",
+        "type": "Jizake",
+        "name": "Cider",
+        "japanese": "多武峯",
+        "quality": 5,
+        "origin": "Japan",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 35
+    },
+    {
+        "id": "art-jizake-red-wine",
+        "type": "Jizake",
+        "name": "Red Wine",
+        "japanese": "赤葡萄酒",
+        "quality": 4,
+        "origin": "Europe",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 36
+    },
+    {
+        "id": "art-jizake-ale",
+        "type": "Jizake",
+        "name": "Ale",
+        "japanese": "泡盛",
+        "quality": 3,
+        "origin": "Japan",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 37
+    },
+    {
+        "id": "art-jizake-mead",
+        "type": "Jizake",
+        "name": "Mead",
+        "japanese": "豊原",
+        "quality": 2,
+        "origin": "Japan",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 38
+    },
+    {
+        "id": "art-jizake-beer",
+        "type": "Jizake",
+        "name": "Beer",
+        "japanese": "麦酒",
+        "quality": 1,
+        "origin": "Europe",
+        "effect": "Skill[Confuse]",
+        "sourceOrder": 39
+    },
+    {
+        "id": "art-scented-wood-agarwood",
+        "type": "Scented Wood",
+        "name": "Agarwood",
+        "japanese": "蘭奢待",
+        "quality": 10,
+        "origin": "Japan",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 40
+    },
+    {
+        "id": "art-scented-wood-princewood",
+        "type": "Scented Wood",
+        "name": "Princewood",
+        "japanese": "太子",
+        "quality": 10,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 41
+    },
+    {
+        "id": "art-scented-wood-dustcloud-wood",
+        "type": "Scented Wood",
+        "name": "Dustcloud Wood",
+        "japanese": "紅塵",
+        "quality": 9,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 42
+    },
+    {
+        "id": "art-scented-wood-rosewood",
+        "type": "Scented Wood",
+        "name": "Rosewood",
+        "japanese": "三芳野",
+        "quality": 9,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 43
+    },
+    {
+        "id": "art-scented-wood-young-agarwood",
+        "type": "Scented Wood",
+        "name": "Young Agarwood",
+        "japanese": "逍遙",
+        "quality": 8,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 44
+    },
+    {
+        "id": "art-scented-wood-nutmeg",
+        "type": "Scented Wood",
+        "name": "Nutmeg",
+        "japanese": "楊貴妃",
+        "quality": 8,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 45
+    },
+    {
+        "id": "art-scented-wood-autumn-maple",
+        "type": "Scented Wood",
+        "name": "Autumn Maple",
+        "japanese": "紅葉賀",
+        "quality": 7,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 46
+    },
+    {
+        "id": "art-scented-wood-aged-cedar",
+        "type": "Scented Wood",
+        "name": "Aged Cedar",
+        "japanese": "花散里",
+        "quality": 7,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 47
+    },
+    {
+        "id": "art-scented-wood-young-rosewood",
+        "type": "Scented Wood",
+        "name": "Young Rosewood",
+        "japanese": "十五夜",
+        "quality": 6,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 48
+    },
+    {
+        "id": "art-scented-wood-late-rain-wood",
+        "type": "Scented Wood",
+        "name": "Late Rain Wood",
+        "japanese": "夕時雨",
+        "quality": 6,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 49
+    },
+    {
+        "id": "art-scented-wood-young-nutmeg",
+        "type": "Scented Wood",
+        "name": "Young Nutmeg",
+        "japanese": "富士煙",
+        "quality": 5,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 50
+    },
+    {
+        "id": "art-scented-wood-harvest-wood",
+        "type": "Scented Wood",
+        "name": "Harvest Wood",
+        "japanese": "般若",
+        "quality": 5,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 51
+    },
+    {
+        "id": "art-scented-wood-young-maple",
+        "type": "Scented Wood",
+        "name": "Young Maple",
+        "japanese": "飛梅",
+        "quality": 4,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 52
+    },
+    {
+        "id": "art-scented-wood-smoky-wood",
+        "type": "Scented Wood",
+        "name": "Smoky Wood",
+        "japanese": "名月",
+        "quality": 4,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 53
+    },
+    {
+        "id": "art-scented-wood-frosty-wood",
+        "type": "Scented Wood",
+        "name": "Frosty Wood",
+        "japanese": "須磨",
+        "quality": 3,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 54
+    },
+    {
+        "id": "art-scented-wood-young-cedar",
+        "type": "Scented Wood",
+        "name": "Young Cedar",
+        "japanese": "隣家",
+        "quality": 3,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 55
+    },
+    {
+        "id": "art-scented-wood-relaxing-wood",
+        "type": "Scented Wood",
+        "name": "Relaxing Wood",
+        "japanese": "霜夜",
+        "quality": 2,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 56
+    },
+    {
+        "id": "art-scented-wood-pinewood",
+        "type": "Scented Wood",
+        "name": "Pinewood",
+        "japanese": "寝覚",
+        "quality": 2,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 57
+    },
+    {
+        "id": "art-scented-wood-beechwood",
+        "type": "Scented Wood",
+        "name": "Beechwood",
+        "japanese": "篠目",
+        "quality": 1,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 58
+    },
+    {
+        "id": "art-scented-wood-balsa-wood",
+        "type": "Scented Wood",
+        "name": "Balsa Wood",
+        "japanese": "上馬",
+        "quality": 1,
+        "origin": "China",
+        "effect": "Skill[Calm]",
+        "sourceOrder": 59
+    }
+];
